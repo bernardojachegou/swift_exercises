5d8c9e2fc8d4fdefb283253e52fd4fe5849b45c1
Michel Bernardo <bernardoenglish@outlook.com>
2021-02-25 14:31:55 -0400

    exercise 04 solved

--- resolutions/4_exercise.swift
+++ resolutions/4_exercise.swift
@@ -1,14 +1,18 @@
 // 4. Faça uma função contadígitos que dados um inteiro n e um inteiro d, 0 < d < 9 (entre 0 e 9), devolve quantas vezes o dígito d aparece em n.
 
-let x: Int = 3222;
+let x: Int = 322234562211;
 let y: Int = 2;
 
-func checkNumber(_ drawnNumber:Int, _ checkedNumber: Int) {
-    let digits = String(drawnNumber).compactMap { Int(String($0)) }
-    print("Digitos: \(digits)") 
+func numberChecker(_ drawnNumber:Int, _ checkedNumber: Int) {
+    let numbers = String(drawnNumber).compactMap { Int(String($0)) };
+    var numberKeeper: [Int] = [];
 
-    // for + if
-    // Se o número y estiver contido em x { imprima ele toda vez que ele aparecer}
+    for number in numbers {
+        if number == checkedNumber {
+            numberKeeper.append(number)
+        }
+    }
+    print("o número \(checkedNumber) aparece \(numberKeeper.count) vezes")
 }
 
-checkNumber(x)
+numberChecker(x, y)
