f773623bdb3451c8d00a8f073b82fa632b408ca3
Michel Bernardo <bernardoenglish@outlook.com>
2021-02-28 10:28:59 -0400

    new solution to exercise 03

--- resolutions/3_exercise.swift
+++ resolutions/3_exercise.swift
@@ -1,84 +1,32 @@
 // 3. Dado um número natural na base binária, transformá-lo para a base decimal.
 // Exemplo: Dado 10010 a saída será 18, pois 1. 2^4 + 0. 2^3 + 0. 2^2 + 1. 2^1 + 0. 2^0 = 18.
 
-//Entrada:
-let binaryNumber = 10010;
-
-// Função para transformar número em array:
-func convertNumberIntoArray(_ number:Int) {
-    let digits = String(number).compactMap { Int(String($0)) }
-    print("dentro da função convertNumberIntoArray: \(digits)") 
-}
-
-// Função para elevar a base com int:
-func handleExpoOfANumber (_ num: Int, _ power: Int) -> Int{
-    return Int(pow(Double(num),Double(power)))
-}
-
-// Função para transofrmar binário em decimal:
-func handleBinaryToDecimal(_ number: Int) {
-
-    // Passo 1 (Transformar em array):
-    let arrayOfNumbers = convertNumberIntoArray(number);
-    print("dentro da função binaryToDecimal: \(arrayOfNumbers)")
-
-    // Passo 2 (Fazer o cálculo de cada número do array):
-    var expoResult = 0;
-    var totalSum = 0;
-    let index = [3, 2, 1, 0];
-    for (index, value) in arrayOfNumbers.enumerated() {
-        expoResult = value * handleExpoOfANumber(2, index);
-        print("o total do expoente em \(index) é \(expoResult)")
-        totalSum = totalSum + expoResult;
-    }
-
-    // Passo 3 (Fazer a soma dos valores e imprimir): 
-    print("resultado em binário: \(totalSum)");
-    
-}
-
-handleBinaryToDecimal(binaryNumber);
-
-////////////////////////////////////////////////////////////////////////
-// Testing:
-
-func handleExpoOfANumber (_ num: Int, _ power: Int) -> Int{
-    return Int(pow(Double(num),Double(power)))
-}
-
-let binaryNumber = [1, 0, 1, 0];
-
-var expoResult = 0;
-var totalSum = 0;
-
-for (index, value) in binaryNumber.enumerated() {
-
-    for index in binaryNumber...0 {
-        expoResult = value * handleExpoOfANumber(2, index);
-    }
-
-    print("o total do expoente em \(index) é \(expoResult)")
-    totalSum = totalSum + expoResult;
- }
-
-
-////////////////////////////////////////////////////////////////////////
-// Outra solução:
+// solução:
 func binaryToDecimal (binary: Int) -> Int? {
-    var newBinary = String(binary);
+    let binaryArray = String(binary);
     var result: Int = 0;
+    var decimal: Int = 0;
 
-    for digit in newBinary {
+    for digit in binaryArray {
         switch(digit) {
             case "0": result = result * 2
             case "1": result = result * 2 + 1
             default: return nil
         }
     }
-    return result;
+    
+    decimal = result;
+    print("O número binário: \(binary) em decimal é: \(decimal)");
+    return decimal;
 }
 
-let binaryNumber = 11201;
-print((binaryToDecimal(binary: binaryNumber)))
+let binary0 = 11;
+let binary1 = 111;
+let binary2 = 110;
+let binary3 = 1111;
 
+(binaryToDecimal(binary: binary0))
+(binaryToDecimal(binary: binary1))
+(binaryToDecimal(binary: binary2))
+(binaryToDecimal(binary: binary3))
 
