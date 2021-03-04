4ede879119b9eab386c5f5ecc726a54c8905fea0
Michel Bernardo <bernardoenglish@outlook.com>
2021-03-04 09:20:21 -0400

    w3resource - exercise 11 solved

---
+++ resolutions/module_02/11_exercise.swift
@@ -0,0 +1,30 @@
+// 11. Write a Swift program to test if an array of integers contains a 3 or a 5.
+let drawnNumbers: [Int] = [1, 2, 4, 6];
+let undrawnNumbers: [Int] = [1, 3, 4, 7];
+let sorteredNumbers: [Int] = [1, 5, 8, 9];
+
+// Solution 01:
+func checkThreeOrFive(collection numbers:[Int]) -> Bool {
+  var finalResult: Bool = false;
+  
+  for number in numbers {
+    if number == 3 || number == 5 {
+      finalResult = true;
+    } 
+  }
+  return finalResult;
+}
+
+print(checkThreeOrFive(collection: drawnNumbers));
+print(checkThreeOrFive(collection: undrawnNumbers));
+print(checkThreeOrFive(collection: sorteredNumbers));
+
+// Solution 02:
+func checkIfContainsThreeOrFive(_ numbers:[Int]) -> Bool {
+  numbers.contains(3) || numbers.contains(5) ? true : false;
+}
+
+print(checkIfContainsThreeOrFive(drawnNumbers));
+print(checkIfContainsThreeOrFive(undrawnNumbers));
+print(checkIfContainsThreeOrFive(sorteredNumbers));
+
