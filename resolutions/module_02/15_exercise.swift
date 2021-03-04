625380a58651c1f31711bae68b2bff842932940a
Michel Bernardo <bernardoenglish@outlook.com>
2021-03-04 16:28:00 -0400

    w3resource - exercise 15 solved

---
+++ resolutions/module_02/15_exercise.swift
@@ -0,0 +1,11 @@
+// 15. Write a Swift program to check if two given arrays of integers have 0 as their first element.
+let drawnNumbers: [Int] = [0, 1, 2, 3];
+let undrawnNumbers: [Int] = [0, 2, 2, 5];
+let sorteredNumbers: [Int] = [1, 2, 3, 4];
+
+func checkIfFirstIsZero(_ x:[Int], _ y:[Int]) -> Bool {
+  x.first! == 0 && y.first! == 0 ? true : false;
+}
+
+print(checkIfFirstIsZero(drawnNumbers, undrawnNumbers)); // true;
+print(checkIfFirstIsZero(drawnNumbers, sorteredNumbers)); // false;
