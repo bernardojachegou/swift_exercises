be275f6ea6d581ebc46044af74bc58705d737ddc
Michel Bernardo <bernardoenglish@outlook.com>
2021-03-05 12:09:36 -0400

    w3resource - exercise 16 solved

---
+++ resolutions/module_02/16_exercise.swift
@@ -0,0 +1,24 @@
+// Write a Swift program to compute the sum of the values of two given array of integers and each length 2.
+// Find the array which has the largest sum and return the first array if the sum of two given arrays are equal.
+let drawnNumbers: [Int] = [1, 2];
+let undrawnNumbers: [Int] = [2, 5];
+
+// Resolution 01:
+func sumTwoAndReturn(_ x:[Int], _ y:[Int]) -> [Int] {
+  guard x.count == 2 && y.count == 2 else {
+    return []
+  }
+
+  let resultOfX = x.reduce(0, +);
+  let resultOfY = y.reduce(0, +);
+
+  if resultOfX == resultOfY || resultOfX > resultOfY {
+    return x;
+  } else {
+    return y;
+  }
+}
+
+print(sumTwoAndReturn(drawnNumbers, undrawnNumbers)); // y
+print(sumTwoAndReturn([1, 1], [1, 1])); // x
+print(sumTwoAndReturn([2, 6], [0, 1])); // x
