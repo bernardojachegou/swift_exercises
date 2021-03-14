8440024c896bc0e2a0ff7813bcb497ded3a01acc
Michel Bernardo <bernardoenglish@outlook.com>
2021-03-14 18:10:34 -0400

    adding last two exercises solution

--- exercises/w3resource/strings/resolutions/10_exercise.swift
+++ exercises/w3resource/strings/resolutions/10_exercise.swift
@@ -1,1 +1,16 @@
 // 10. Write a Swift program to create a new string of length three from a given string of odd length from its middle. The string length must be three.
+func middle_three_char(_ input: String) -> String {
+    let chars = input.characters
+     let middle_num = (chars.count - 1) / 2
+    let first_num = middle_num - 1
+    let third_num = middle_num + 1
+    
+    let first_index = input.index(chars.startIndex, offsetBy: first_num)
+    let third_index = input.index(chars.startIndex, offsetBy: third_num + 1)
+    let middle_range = first_index ..< third_index
+    let result = input.substring(with: middle_range)
+    
+    return result
+}
+print(middle_three_char("Swift"))
+print(middle_three_char("abcde"))
--- exercises/w3resource/strings/resolutions/9_exercise.swift
+++ exercises/w3resource/strings/resolutions/9_exercise.swift
@@ -1,1 +1,19 @@
-// 9. Write a Swift program to create a string made of two copies of the last two characters of a given string. The given string length must be at least 2.
+// 9. Write a Swift program to create a string made of two copies of the last two characters of a given string. 
+// The given string length must be at least 2.
+
+func GenerateNewString(_ text: String) -> String {
+    var newString: String = "";
+    var lastTwo = text.endIndex;
+    
+    for _ in 0..<2 {
+      lastTwo = text.index(before: lastTwo)
+    }
+    let lastTwoChars = text.substring(from: lastTwo)
+    for _ in 0..<2 {
+      newString.append(lastTwoChars)
+    }
+    
+    return newString
+}
+print(GenerateNewString("Michel")); // elel
+print(GenerateNewString("Aline")); // nene
