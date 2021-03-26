9698b5b52c96f77e891ea56886ba4917d7691523
Michel Bernardo <bernardoenglish@outlook.com>
2021-03-26 14:26:56 -0400

    adding some informations about init in Swift

--- swift_docs/classes.swift
+++ swift_docs/classes.swift
@@ -1,8 +1,24 @@
+// ---------------------------------------- Class features
+class Person {
+  // Property
+  var name = ""
+}
+
 class BlogPost {
+  // Computed property
+  var fullTitle: String {
+    if title != nil && author != nil {
+      return title! + " by " + author!.name
+    } else if title != nil {
+      return title!
+    } else {
+      return "No title"
+    }
+  }
   // Properties
-  var title = ""
-  var body = ""
-  var author = ""
+  var title: String?
+  var body = "hey"
+  var author: Person?
   var numberOfComments = 0
 
   //Methods
@@ -12,12 +28,15 @@ class BlogPost {
 }
 
 // Class instance (Object)
+let author = Person()
+// Adding name:
+author.name = "Michel Bernardo"
+// Class instance (Object)
 let myPost = BlogPost()
-myPost.title = "Learning Swift"
-myPost.body = "Some description about Swift learning"
-myPost.author = "Michel Bernardo"
-myPost.addComment()
-print(myPost.numberOfComments);
+// Adding author:
+myPost.author = author;
+//Adding title:
+myPost.title = "Title"
 
 // ---------------------------------------- Inheritance
 class Car {
@@ -29,8 +48,10 @@ class Car {
 }
 class SuperCar: Car {
 
+  // Using the Car (super class) method:
   override func drive() {
     super.drive()
+    // Adding new information:
     print("and rockets boosting at 50")
   }
 
@@ -38,9 +59,24 @@ class SuperCar: Car {
     print("Flying")
   }
 }
+
 let myCar = Car()
 myCar.drive()
 
 let mySuperCar = SuperCar()
 mySuperCar.drive()
 mySuperCar.fly()
+
+// ---------------------------------------- Self in init
+class Player{
+  var name: String = ""
+  var age: Int = 29
+
+  init(_ name:String, _ age:Int) {
+    self.name = name
+    self.age = age
+  }
+}
+
+let me = Player("Michel", 29);
+let you = Player("Davi", 25);
